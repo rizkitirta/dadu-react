57aeb82073849ff3a92be72d78d99c413799f4ad
rizkitirta <rizkibuwono0@gmail.com>
2022-06-09 22:08:58 +0700

    update page

--- src/pages/game-play/index.jsx
+++ src/pages/game-play/index.jsx
@@ -13,21 +13,21 @@ function Game() {
 
   const exitGame = () => {
     if (window.confirm("Are you sure you want to exit?")) {
-      localStorage.setItem('added_player', false)
-      localStorage.setItem('id_game', "")
+      localStorage.setItem("added_player", false);
+      localStorage.setItem("id_game", "");
       navigate("/");
     }
   };
 
   useEffect(() => {
-    setAddedPlayer()
-    console.log(player)
-    getPlayer()
-  }, [])
+    setAddedPlayer();
+    console.log(player);
+    getPlayer();
+  }, []);
 
   const getPlayer = () => {
     axios
-      .get(API+ "/permainan/player-dadu/"+id, {
+      .get(API + "/permainan/player-dadu/" + id, {
         headers: {
           Authorization: "Bearer " + localStorage.getItem("token"),
         },
@@ -38,41 +38,43 @@ function Game() {
       .catch((err) => {
         console.log(err);
       });
-  }
-  
+  };
+
   const setAddedPlayer = () => {
-    const addedPlayer = localStorage.getItem('added_player')
-    const idGame = localStorage.getItem('id_game')
+    const addedPlayer = localStorage.getItem("added_player");
+    const idGame = localStorage.getItem("id_game");
     if (addedPlayer && idGame === id) {
-      setPlayer(true)
-    }else{
-      setPlayer(false)
+      setPlayer(true);
+    } else {
+      setPlayer(false);
     }
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post(
-      API +"/permainan/store/player-dadu",
-      {
-        jumlah_pemain: jumlahPemain,
-        jumlah_dadu: jumlahDadu,
-        permainan_id: id,
-      },
-      {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("token"),
+    axios
+      .post(
+        API + "/permainan/store/player-dadu",
+        {
+          jumlah_pemain: jumlahPemain,
+          jumlah_dadu: jumlahDadu,
+          permainan_id: id,
         },
-      }
-    ).then((res) => {
-      console.log(res);
-      setPlayer(true)
-      localStorage.setItem('added_player', true)
-      localStorage.setItem('id_game', id)
-    }).catch((err) => {
-      console.log(err);
-    });
-    
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        setPlayer(true);
+        localStorage.setItem("added_player", true);
+        localStorage.setItem("id_game", id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -103,7 +105,7 @@ function Game() {
                 data-mdb-ripple-color="light"
               >
                 Buat Player
-              </label> 
+              </label>
               <a
                 className="inline-block ml-2 px-7 py-3 mb-1 border-2 border-gray-200 text-gray-200 font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
                 href="#!"
@@ -118,9 +120,29 @@ function Game() {
           </div>
         </div>
       </div>
-      {/* Jumbotron */}
-      <div className="flex justify-center">
-        <div className="block p-5 rounded-lg shadow-2xl bg-white max-w-sm grid grid-cols-3 gap-2">
+
+      <h1 className="font-bold text-2xl my-10">Maintenance!</h1>
+      <img src="/dadu.png" className="max-w-sm  mx-auto my-20 animate-bounce mt-36" />
+
+      {/* Dadu */}
+      <div className="flex justify-center gap-4 my-10">
+        <div className="block p-5 rounded-lg shadow-2xl bg-white max-w-sm grid grid-cols-3 gap-2 animate-pulse">
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+        </div>
+        <div className="block p-5 rounded-lg shadow-2xl bg-white max-w-sm grid grid-cols-3 gap-2 animate-pulse">
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+          <div className="rounded-full bg-black block h-10 w-10"></div>
+        </div>
+        <div className="block p-5 rounded-lg shadow-2xl bg-white max-w-sm grid grid-cols-3 gap-2 animate-pulse">
           <div className="rounded-full bg-black block h-10 w-10"></div>
           <div className="rounded-full bg-black block h-10 w-10"></div>
           <div className="rounded-full bg-black block h-10 w-10"></div>
@@ -129,8 +151,6 @@ function Game() {
           <div className="rounded-full bg-black block h-10 w-10"></div>
         </div>
       </div>
-
-      <img src="/dadu.png" className="max-w-sm shake" />
 
       {/* Modal */}
       <>
